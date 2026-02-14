c9b057a68e12c19c41dc17f1c2f8a1ed3e78d01c
dhyanesht <youremail@example.com>
2026-02-14 17:22:43 -0500

    initialized with mono repo. --wip

---
+++ apps/java/src/main/java/com/dino/inbox_track/InboxTrackApplication.java
@@ -0,0 +1,13 @@
+package com.dino.inbox_track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InboxTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InboxTrackApplication.class, args);
+	}
+
+}
---
+++ apps/java/src/test/java/com/dino/inbox_track/InboxTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dino.inbox_track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InboxTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
